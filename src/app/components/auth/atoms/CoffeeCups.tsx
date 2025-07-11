2a5443a15bbe8197ce037f541985223b748bd9db
esteban2841 <estebanpuentesdev@gmail.com>
2025-07-11 00:20:55 -0500

    fix: fixing routing types

--- src/app/components/auth/atoms/CoffeeCups.tsx
+++ src/app/components/auth/atoms/CoffeeCups.tsx
@@ -1,8 +1,8 @@
 'use client'
-import { useRef } from 'react'
+import * as THREE from 'three'
 import { useGLTF } from '@react-three/drei'
 
-export function CoffeeCups(props) {
+export function CoffeeCups(props: any) {
   const { nodes, materials } = useGLTF('/models/coffee/scene.gltf')
   return (
     <group {...props} dispose={null}>
@@ -10,7 +10,7 @@ export function CoffeeCups(props) {
         <mesh
           castShadow
           receiveShadow
-          geometry={nodes.coffee_cappuccino_03_coffee_BLACK_0.geometry}
+          geometry={(nodes.coffee_cappuccino_03_coffee_BLACK_0 as THREE.Mesh).geometry}
           material={materials.coffee_BLACK}
           rotation={[-Math.PI / 2, 0, 0]}
           scale={100}
@@ -18,7 +18,7 @@ export function CoffeeCups(props) {
         <mesh
           castShadow
           receiveShadow
-          geometry={nodes.coffee_blackEsspresso_03_coffee_BLACK_0.geometry}
+          geometry={(nodes.coffee_blackEsspresso_03_coffee_BLACK_0 as THREE.Mesh).geometry}
           material={materials.coffee_BLACK}
           position={[34.348, 0, 0]}
           rotation={[-Math.PI / 2, 0, 0]}
@@ -27,7 +27,7 @@ export function CoffeeCups(props) {
         <mesh
           castShadow
           receiveShadow
-          geometry={nodes.coffee_latte_03_coffee_BLACK_0.geometry}
+          geometry={(nodes.coffee_latte_03_coffee_BLACK_0 as THREE.Mesh).geometry}
           material={materials.coffee_BLACK}
           position={[-19.272, 0, 0]}
           rotation={[-Math.PI / 2, 0, 0]}
@@ -36,7 +36,7 @@ export function CoffeeCups(props) {
         <mesh
           castShadow
           receiveShadow
-          geometry={nodes.coffee_blackEsspresso_02_coffee_BLACK_0.geometry}
+          geometry={(nodes.coffee_blackEsspresso_02_coffee_BLACK_0 as THREE.Mesh).geometry}
           material={materials.coffee_BLACK}
           position={[34.348, 0, 0]}
           rotation={[-Math.PI / 2, 0, 0]}
@@ -45,7 +45,7 @@ export function CoffeeCups(props) {
         <mesh
           castShadow
           receiveShadow
-          geometry={nodes.coffee_black_02_coffee_BLACK_0.geometry}
+          geometry={(nodes.coffee_black_02_coffee_BLACK_0 as THREE.Mesh).geometry}
           material={materials.coffee_BLACK}
           position={[19.386, 0, 0]}
           rotation={[-Math.PI / 2, 0, 0]}
@@ -54,7 +54,7 @@ export function CoffeeCups(props) {
         <mesh
           castShadow
           receiveShadow
-          geometry={nodes.coffee_latte_02_coffee_LATTE_0.geometry}
+          geometry={(nodes.coffee_latte_02_coffee_LATTE_0 as THREE.Mesh).geometry}
           material={materials.coffee_LATTE}
           position={[-19.272, 0, 0]}
           rotation={[-Math.PI / 2, 0, 0]}
@@ -63,7 +63,7 @@ export function CoffeeCups(props) {
         <mesh
           castShadow
           receiveShadow
-          geometry={nodes.coffee_cappuccino_02_coffee_BLACK_0.geometry}
+          geometry={(nodes.coffee_cappuccino_02_coffee_BLACK_0 as THREE.Mesh).geometry}
           material={materials.coffee_BLACK}
           rotation={[-Math.PI / 2, 0, 0]}
           scale={100}
@@ -71,7 +71,7 @@ export function CoffeeCups(props) {
         <mesh
           castShadow
           receiveShadow
-          geometry={nodes.coffee_blackEsspresso_01_coffee_BLACK_0.geometry}
+          geometry={(nodes.coffee_blackEsspresso_01_coffee_BLACK_0 as THREE.Mesh).geometry}
           material={materials.coffee_BLACK}
           position={[34.348, 0.344, 0]}
           rotation={[-Math.PI / 2, 0, 0]}
@@ -80,7 +80,7 @@ export function CoffeeCups(props) {
         <mesh
           castShadow
           receiveShadow
-          geometry={nodes.coffee_black_01_coffee_BLACK_0.geometry}
+          geometry={(nodes.coffee_black_01_coffee_BLACK_0 as THREE.Mesh).geometry}
           material={materials.coffee_BLACK}
           position={[19.386, 5.141, 0]}
           rotation={[-Math.PI / 2, 0, 0]}
@@ -89,7 +89,7 @@ export function CoffeeCups(props) {
         <mesh
           castShadow
           receiveShadow
-          geometry={nodes.coffee_latte_01_coffee_LATTE_0.geometry}
+          geometry={(nodes.coffee_latte_01_coffee_LATTE_0 as THREE.Mesh).geometry}
           material={materials.coffee_LATTE}
           position={[-19.272, 6.195, 0]}
           rotation={[-Math.PI / 2, 0, Math.PI / 2]}
@@ -98,7 +98,7 @@ export function CoffeeCups(props) {
         <mesh
           castShadow
           receiveShadow
-          geometry={nodes.coffee_cappuccino_01_coffee_CAPPUCCINO_0.geometry}
+          geometry={(nodes.coffee_cappuccino_01_coffee_CAPPUCCINO_0 as THREE.Mesh).geometry}
           material={materials.coffee_CAPPUCCINO}
           position={[0, 0.922, 0]}
           rotation={[-Math.PI / 2, 0, 0]}
